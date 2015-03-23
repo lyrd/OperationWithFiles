0c56ded8dc1f29014cd058be44fb9334fe2efb1f
lyrd <antonmol-92@yandex.ru>
2015-03-23 16:36:17 +0800

    v.1.0.0.0

---
+++ IniFile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationWithFiles
+{
+    public class IniFile
+    {
+        public string path;
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section,
+        string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section,
+        string key, string def, StringBuilder retVal,
+        int size, string filePath);
+
+        [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileSectionNamesA")]
+        static extern int GetPrivateProfileSectionNames(byte[] lpszReturnBuffer, int nSize, string lpFileName);
+
+        public IniFile(string INIPath)
+        {
+            path = INIPath;
+        }
+
+        public void IniWriteValue(string Section, string Key, string Value)
+        {
+            WritePrivateProfileString(Section, Key, Value, this.path);
+        }
+
+
+        public string IniReadValue(string Section, string Key)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(Section, Key, "", temp,
+            255, this.path);
+            return temp.ToString();
+        }
+
+        public byte[] IniSection()
+        {
+            byte[] buffer = new byte[1024];
+            GetPrivateProfileSectionNames(buffer, buffer.Length, this.path);
+            return buffer;
+        }
+    }
+}
